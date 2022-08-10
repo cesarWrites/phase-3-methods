a442790cfe2c8759e555a960bd8a27f0b1dadc51
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-10 22:54:27 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+    end
+
+greet("Jimmy")
+greet("Naureen")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default()
+
+def add num1, num2
+    puts num1
+    puts num2
+    num1 + num2
+end
+
+puts add(1,6)
+
+def halve num
+    if num.class != Integer
+      return 
+    end
+
+    num /2 
+end
+
+halve (6)
+
